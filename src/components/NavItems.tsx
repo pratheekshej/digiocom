b3c077b6d003a0ee81608f9533b710687781ddc4
Pratheeksh Joseph <pratheekshej@gmail.com>
2025-03-06 15:31:56 -0500

    DC-004: Updating Navitems toggle functionality for ESC key

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
 import { useOnClickOutside } from "@/hooks/useOnClickOutsise";
 
@@ -10,6 +10,18 @@ const NavItems = () => {
 
     const navRef = useRef<HTMLDivElement>(null!);
     useOnClickOutside(navRef, () => setActiveIndex(null));
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === "Escape") {
+                setActiveIndex(null);
+            }
+        };
+        document.addEventListener('keydown', handler);
+        return () => {
+            document.removeEventListener('keydown', handler);
+        }
+    }, [])
 
     return (
         <div className="flex gap-4 h-full" ref={navRef}>
